160aa87136493a45092640f0d27b58fc0a028c30
JoyalMPaul <joyalmathewpaul@gmail.com>
2025-05-22 19:04:35 -0400

    updated

--- django/website/my_website/settings.py
+++ django/website/my_website/settings.py
@@ -16,6 +16,9 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#from django.core.management.utils import get_random_secret_key
+#print(get_random_secret_key())
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
